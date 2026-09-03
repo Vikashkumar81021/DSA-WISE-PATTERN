e3a90a6bd7d24912febf17cbcd8f3c2fbc297c9d
Vikashkumar81021 <viku81021@gmail.com>
2026-09-03 07:05:21 +0530

    add linkedlist code

--- LinkedList/deleteOperation.js
+++ LinkedList/deleteOperation.js
@@ -5,61 +5,95 @@ class Node {
   }
 }
 let head = new Node(10);
+let newNode = new Node(20);
+head.next = newNode;
+// newNode.next = head;
+// console.log("head", newNode);
 
-function insertStart(head, data) {
-  let newNode = new Node(data);
-  newNode.next = head;
-  return newNode;
-}
-head = insertStart(head, 5); //iska mtlb hua head=new node function hmne head k andr call kr diya
-
-function deleteStart(head) {
+function deleteLastNode(head) {
   if (head === null) {
-    console.log("LinkedList is Empty");
-    return;
+    return null;
   }
-  return head.next;
-}
-// head = deleteStart(head);
-function deleteAtEnd(head) {
-  let temp = head;
-  while (temp.next.next !== null) {
-    temp = temp.next;
-  }
-  temp.next = null;
-  return head;
-}
-// head = deleteAtEnd(head);
-// function deleteAtIndex(head, target) {
-//   let index = 0;
-//   let ref = head;
-//   while (ref.next !== null && index !== target - 1) {
-//     ref = ref.next;
-//   }
-//   ref.next = ref.next.next;
-//   return head;
-// }
-// head = deleteAtIndex(head, 1);
-//this line without function
-// head = head.next;
-
-function deleteIndex(head, tar) {
-  if (head == null) return null;
-  //agar pahle node delet krnaho
-  if (head.data === tar) {
-    return head.next;
+  if (head.next === null) {
+    return null;
   }
   let curr = head;
   let prev = null;
   while (curr.next !== null) {
     prev = curr;
     curr = curr.next;
-    if (curr.data === tar) {
-      prev.next = curr.next;
-      return head;
-    }
   }
+  prev.next = null;
   return head;
 }
-head = deleteIndex(head, 5);
-console.log(head);
+
+// head = deleteLastNode(head);
+console.log("head", head);
+function deleteStart(head) {
+  if (head === null) {
+    console.log("Linedlist is Empty");
+    return;
+  }
+  return head.next;
+}
+// head = deleteStart(head);
+
+function deleteParticularNode() {}
+
+// function insertStart(head, data) {
+//   let newNode = new Node(data);
+//   newNode.next = head;
+//   return newNode;
+// }
+// head = insertStart(head, 5); //iska mtlb hua head=new node function hmne head k andr call kr diya
+
+// function deleteStart(head) {
+//   if (head === null) {
+//     console.log("LinkedList is Empty");
+//     return;
+//   }
+//   return head.next;
+// }
+// // head = deleteStart(head);
+// function deleteAtEnd(head) {
+//   let temp = head;
+//   while (temp.next.next !== null) {
+//     temp = temp.next;
+//   }
+//   temp.next = null;
+//   return head;
+// }
+// // head = deleteAtEnd(head);
+// // function deleteAtIndex(head, target) {
+// //   let index = 0;
+// //   let ref = head;
+// //   while (ref.next !== null && index !== target - 1) {
+// //     ref = ref.next;
+// //   }
+// //   ref.next = ref.next.next;
+// //   return head;
+// // }
+// // head = deleteAtIndex(head, 1);
+// //this line without function
+// // head = head.next;
+
+// function deleteIndex(head, tar) {
+//   if (head == null) return null;
+//   //agar pahle node delet krnaho
+//   if (head.data === tar) {
+//     return head.next;
+//   }
+//   let curr = head;
+//   let prev = null;
+//   while (curr.next !== null) {
+//     prev = curr;
+//     curr = curr.next;
+//     if (curr.data === tar) {
+//       prev.next = curr.next;
+//       return head;
+//     }
+//   }
+//   return head;
+// }
+// head = deleteIndex(head, 5);
+// console.log(head);
--- LinkedList/test.js
+++ LinkedList/test.js
@@ -23,7 +23,8 @@ function insertAtEnd(head) {
   curr.next = tail;
   return head;
 }
-// head = insertAtEnd(head)
+// head = insertAtEnd(head);
+
 function deleteAtEnd(head) {
   let curr = head;
   while (curr.next.next !== null) {
